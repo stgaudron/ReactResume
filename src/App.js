634cef564b9452cd3dfccafe49e47ddfb80c84c6
stgaudron <60404254+stgaudron@users.noreply.github.com>
2020-06-17 17:53:43 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import {
 } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import MiniDrawer from './components/drawer';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Games from './components/Games';
+import { makeStyles } from '@material-ui/core/styles';
 import ContactInfo from './components/Contact';
 import GitHub from './components/GitHub';
 import Resume from './components/Resume';
@@ -19,67 +18,6 @@ import Skills from './components/Skills';
 let homeimage = require('./home.jpg')
 const drawerWidth = 240;
 
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
 
 
 function App() {
@@ -106,10 +44,5 @@ function App() {
      }
    return <img src={homeimage} alt="home" style={{height:'auto',width:'100%'}} onClick={handleClick} />;
  }
-
-
-
-
-
 
 export default App;
